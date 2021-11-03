0375c24c3e277c50f7297a7c80a40859b678b79d
thecarpetjasp <71789855+thecarpetjasp@users.noreply.github.com>
2021-11-03 19:32:41 -0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,7 @@
+import os
+os.system("cls||clear")
+os.system("sudo pip3 install colorama")
+os.system("sudo chmod +x urlmask.py")
+os.system("sudo mv urlmask.py /usr/bin/urlmask")
+os.system("cls||clear")
+print ("Setup complete. Type \'urlmask\' anywhere in your terminal to launch URLMask.")
